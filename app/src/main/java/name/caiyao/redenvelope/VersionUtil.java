b5e80a2f6ef9361f6d99c53bfa9ffd9464ab762b
xiaomu <whucaiyao@gmail.com>
2017-12-20 16:28:50 +0800

    还没更新

--- app/src/main/java/name/caiyao/redenvelope/VersionUtil.java
+++ app/src/main/java/name/caiyao/redenvelope/VersionUtil.java
@@ -9,17 +9,6 @@ public class VersionUtil {
     public static String QQPluginClass = "com.tenpay.android.qqplugin.a.p";
     public static void init(String version) {
         switch (version) {
-            case "6.2.0":
-            case "6.2.1":
-            case "6.2.3":
-            case "6.3.1":
-            case "6.3.3":
-            case "6.3.5":
-            case "6.3.6":
-            case "6.3.7":
-            case "6.5.0":
-            case "6.5.3":
-            case "6.5.5":
             case "6.5.8":
                 QQPluginClass = "com.tenpay.android.qqplugin.a.p";
                 break;
